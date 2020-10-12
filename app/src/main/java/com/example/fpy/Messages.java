dddb67fefd524ea91bfe430742fe08c1aac261f0
JiaweiGit1999 <101209798@students.swinburne.edu.my>
2020-10-12 19:41:21 +0800

    initial live chat

---
+++ app/src/main/java/com/example/fpy/Messages.java
@@ -0,0 +1,40 @@
+package com.example.fpy;
+
+import java.sql.Time;
+import java.sql.Timestamp;
+
+public class Messages {
+    private String name;
+    private String message;
+    private Timestamp timeStamp;
+
+    public Messages(String name, String message, Timestamp timeStamp) {
+        this.name = name;
+        this.message = message;
+        this.timeStamp = timeStamp;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public void setTimeStamp(Timestamp timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public Timestamp getTimeStamp() {
+        return timeStamp;
+    }
+}

4055c76dcd32a4dd47f3d1435b762f69660f6d12
JiaweiGit1999 <101209798@students.swinburne.edu.my>
2020-11-05 19:52:29 +0800

    added only can book timeslot if not taken

--- app/src/main/java/com/example/fpy/Time_facility.java
+++ app/src/main/java/com/example/fpy/Time_facility.java
@@ -1,5 +1,6 @@
 package com.example.fpy;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -15,7 +16,16 @@ import android.widget.Spinner;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
 import java.sql.Time;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -24,7 +34,9 @@ import java.util.Locale;
 public class Time_facility extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     private String duration;
     private Booking booking;
-
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    CollectionReference docRef = db.collection("booking");
+    Boolean valid_date = true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,6 +46,8 @@ public class Time_facility extends AppCompatActivity implements AdapterView.OnIt
         if (getbundle != null) {
             booking = getbundle.getParcelable("booking");
         }
+
+
 
         final Spinner spinner = findViewById(R.id.spinner);
         final TimePicker timePicker = findViewById(R.id.time_picker);
@@ -62,15 +76,60 @@ public class Time_facility extends AppCompatActivity implements AdapterView.OnIt
         confirmbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(Time_facility.this, booking_detail.class);
-                Bundle bundle = new Bundle();
+
                 int hour = timePicker.getHour();
                 int minute = timePicker.getMinute();
                 booking.setTime(String.format(Locale.ENGLISH, "%02d", hour) + ":" + String.format(Locale.ENGLISH, "%02d", minute));
                 booking.setDuration(duration);
-                bundle.putParcelable("booking", booking);
-                intent.putExtras(bundle);
-                startActivity(intent);
+                final SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy HH:mm");
+                valid_date = true;
+                docRef.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        for (QueryDocumentSnapshot document : task.getResult()) {
+                            Long slottaken = 0L;
+                            Long takensoon = 5L;
+                            String stringduration = document.getString("duration").replace(" hours","");
+                            Long duration = Long.parseLong(stringduration);
+                            if(document.getString("facility") == booking.getFacility()) {
+                                try {
+
+                                    String selectedtime = booking.getDate()+" "+booking.getTime();
+                                    String bookedtime = document.get("date")+" "+document.get("time");
+
+                                    Date selecteddate = format.parse(selectedtime);
+                                    Date bookeddate = format.parse(bookedtime);
+
+                                    slottaken = miliTohour(bookeddate,selecteddate);
+                                    takensoon = miliTohour(selecteddate,bookeddate);
+
+                                    Log.d("Date: ",bookedtime);
+                                    Log.d("Date: ",duration.toString());
+                                    Log.d("Date: ",slottaken.toString());
+                                    Log.d("Date: ",takensoon.toString());
+                                } catch (ParseException e) {
+                                    e.printStackTrace();
+                                }
+                                if(slottaken < duration|| takensoon < duration){
+                                    valid_date = false;
+                                }
+                            }
+                        }
+                        if(valid_date){
+                            Log.d("Date: ", "Date is valid");
+                            Intent intent = new Intent(Time_facility.this, booking_detail.class);
+                            Bundle bundle = new Bundle();
+                            bundle.putParcelable("booking", booking);
+                            intent.putExtras(bundle);
+                            startActivity(intent);
+                        }else{
+                            Log.d("Date: ", "Date is not valid");
+                            Toast.makeText(getApplicationContext(),"Time slot is not available for booking.",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+
             }
         });
 
@@ -84,5 +143,14 @@ public class Time_facility extends AppCompatActivity implements AdapterView.OnIt
     @Override
     public void onNothingSelected(AdapterView<?> adapterView) {
 
+    }
+
+    public long miliTohour(Date book,Date select){
+        long diff = book.getTime() - select.getTime();
+        long seconds = diff / 1000;
+        long minutes = seconds / 60;
+        long hours = minutes / 60;
+
+        return Math.abs(hours);
     }
 }
